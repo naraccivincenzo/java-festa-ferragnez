d16b0ce3aedaa66df4edca3faaa78eb0cf8fe0b9
naracci.vincenzo <143946955+naraccivincenzo@users.noreply.github.com>
2024-06-06 22:04:05 +0200

    exercise done with for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,9 +1,37 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
-
+		
+		String [] invited = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Inserisci il tuo nome per scoprire se sei invitato");
+		String legalName = input.nextLine();
+		
+		input.close();
+		
+		for (int i=0; i<invited.length; i++) {
+			if (legalName.toLowerCase().equals(invited[i].toLowerCase())) {
+				System.out.println(invited[i]);
+				System.out.println("Complimenti parteciperai alla festa di divorzio");
+				break;
+			}
+				else {
+				System.out.println("Mi dispiace ma per te niente Pandoro");
+				break;
+			}
+		}
+		
+		
+		
+		
+		
+		
 	}
 
 }
